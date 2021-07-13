12266a343c322fd90147caf4bf859f50e4479cf9
Wenz022 <satech.hjwoo@gmail.com>
2021-07-13 10:52:40 +0900

    2021-07-13 10:52

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hjwoo:1234@cluster0.a8bf0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//application/json
+app.use(bodyParser.json());
 
+mongoose
+  .connect(
+    "mongodb+srv://hjwoo:1234@cluster0.a8bf0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+// 회원가입 route
+app.post("/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어진다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
