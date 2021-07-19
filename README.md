a3610ea80d891fdd8a9f2d95fd1bc4d5b5285c87
Wenz022 <satech.hjwoo@gmail.com>
2021-07-19 19:27:40 +0900

    2021-07-19 19:27

---
+++ README.md
@@ -0,0 +1,5 @@
+# REACT EXAMPLE 02
+
+jaewonhimnae님의 boiler-plate-ko 예제입니다. :sparkles:
+유튜부 링크 (https://www.youtube.com/watch?v=fgoMqmNKE18&list=PL9a7QRYt5fqkZC9jc7jntD1WuAogjo_9T)
+깃허브 주소 (https://github.com/jaewonhimnae/boiler-plate-ko)
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+//salt를 먼저 생성 -> salt를 이용해서 비밀번호를 암호화 해야함.
+const saltRounds = 10; //salt가 몇글자인지
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +32,16 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  //비밀번호를 암호화 시킨다.
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+      //Stroe hash in your password DB.
+    });
+  });
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
